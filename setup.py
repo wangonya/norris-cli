c2c6972d94f249f05d2512718b6744724c25feab
Kinyanjui Wangonya <kwangonya@gmail.com>
2018-12-26 21:11:33 +0300

    update setup

--- setup.py
+++ setup.py
@@ -1,17 +1,13 @@
-import pathlib
 from setuptools import setup, find_packages
 
-# The directory containing this file
-HERE = pathlib.Path(__file__).parent
-
-# The text of the README file
-README = (HERE / "README.md").read_text()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='norris-cli',
     version='0.1',
     description="A simple cli app that brightens up your day with Chuck Norris jokes.",
-    long_description=README,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/wangonya/norris-cli",
     author="Kinyanjui Wangonya",
@@ -23,7 +19,6 @@ setup(
         "Programming Language :: Python :: 3.7",
     ],
     packages=find_packages(),
-    py_modules=['norris-cli'],
     include_package_data=True,
     install_requires=[
         'Click',
