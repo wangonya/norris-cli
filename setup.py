66051ed73017a1c36a21d6416a51787e75857ddd
Kinyanjui Wangonya <kwangonya@gmail.com>
2018-12-27 07:47:58 +0300

    update setup configs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='norris-cli',
-    version='0.1',
+    version='2.0',
     description="A simple cli app that brightens up your day with Chuck Norris jokes.",
     long_description=long_description,
     long_description_content_type="text/markdown",
